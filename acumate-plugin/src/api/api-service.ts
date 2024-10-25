2475794f961fc00164a0d470ff727daa8d640361
alexander.nesvizhsky <alexander.nesvizhsky@acumatica.com>
2024-10-25 18:35:24 +0200

    Axios replaced with fetch

--- acumate-plugin/src/api/api-service.ts
+++ acumate-plugin/src/api/api-service.ts
@@ -1,46 +1,10 @@
-import axios from "axios";
 import { GraphModel } from "../model/graph-model";
-import { json } from "stream/consumers";
 import { GraphStructure } from "../model/graph-structure";
 import { GraphAPIRoute, GraphAPIStructureRoute, AuthEndpoint, LogoutEndpoint } from "./constants";
 import { IAcuMateApiClient } from "./acu-mate-api-client";
 import { AcuMateContext } from "../plugin-context";
 
 export class AcuMateApiClient implements IAcuMateApiClient {
-    
-    private client: axios.AxiosInstance; 
-
-    constructor()
-    {
-        axios.defaults.withCredentials = true;
-        axios.defaults.headers.common = {
-            "Content-Type": "application/json"
-        };
-        axios.defaults.baseURL = AcuMateContext.ConfigurationService.backedUrl;
-        // Add a request interceptor
-        axios.interceptors.request.use(request => {
-            console.log('Starting Request', request);
-            return request;  // Make sure to return the request object
-        }, error => {
-            console.error('Request error', error);
-            return Promise.reject(error);
-        });
-        this.client = axios.create();
-    }
-    
-
-    private getTrasformRequest() {
-        return {
-            transformRequest: [
-                (data: any) => {
-                    if (data) {
-                        return JSON.stringify(data);
-                    }
-                    return undefined;
-                }
-            ]
-        };
-    }
 
     private async auth() {
         const data = {
@@ -48,44 +12,20 @@ export class AcuMateApiClient implements IAcuMateApiClient {
             "password" : AcuMateContext.ConfigurationService.password,
             "tenant" : AcuMateContext.ConfigurationService.tenant
         };
-        return await this.client.post(AuthEndpoint, data, this.getTrasformRequest());
+        return await fetch(AcuMateContext.ConfigurationService.backedUrl!+AuthEndpoint, {
+            method:'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data),
+            credentials: `include`
+        });
     }
 
     private async logout(): Promise<void> {
-        await this.client.post(LogoutEndpoint);
-    }
-
-    private async makePostRequest<T>(route: string, data: any): Promise<T | undefined> {
-        if (!AcuMateContext.ConfigurationService.useBackend) {
-            return undefined;
-        }
-
-        try {
-            if (AcuMateContext.ConfigurationService.useAuthentification) {
-                const authResponse = await this.auth();
-
-                if (authResponse.status !== 200 && authResponse.status !== 204) {
-                    return undefined;
-                }
-            }
-
-            const response = await this.client.post(route, {
-                data: data
-            }, this.getTrasformRequest());
-
-            console.log(response.data);
-
-            return response.data as T;
-        }
-        catch (error) {
-            console.error('Error making POST request:', error);
-            return undefined;
-        }
-        finally {
-            if (AcuMateContext.ConfigurationService.useAuthentification) {
-                await this.logout();
-            }
-        }
+        await fetch(AcuMateContext.ConfigurationService.backedUrl!+LogoutEndpoint, {
+            method:'POST',
+            headers: { "Content-Type": "application/json" },
+            credentials: `include`
+        });
     }
 
     private async makeGetRequest<T>(route: string): Promise<T | undefined> {
@@ -102,11 +42,22 @@ export class AcuMateApiClient implements IAcuMateApiClient {
                 }
             }
 
-            const response = await this.client.get(route );
+            const url = AcuMateContext.ConfigurationService.backedUrl!+route;
+            const settings: RequestInit = {
+                method:'POST',
+                headers: { "Content-Type": "application/json" },
+                
+            };
+            if (AcuMateContext.ConfigurationService.useAuthentification) {
+                settings.credentials = `include`;
+            }
+            const response = await fetch(url, );
 
-            console.log(response.data);
+            const data = await response.json();
 
-            return response.data as T;
+            console.log(data);
+
+            return data as T;
         }
         catch (error) {
             console.error('Error making POST request:', error);
